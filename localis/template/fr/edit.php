0c977531f31c216c425fab0cb145eb817fe1b37a
mose <mose>
2003-02-04 06:00:09 +0000

    created edit language file

---
+++ localis/template/fr/edit.php
@@ -0,0 +1,9 @@
+<?
+$GLOBALS['texteedittitre'] = "Ajout de Point";
+$GLOBALS['texteeditlayer'] = "Calque";
+$GLOBALS['texteeditstatus'] = "Status";
+$GLOBALS['texteeditsubject'] = "Sujet";
+$GLOBALS['texteeditdesc'] = "Description";
+$GLOBALS['texteeditsign'] = "Signature";
+$GLOBALS['texteeditsubmit'] = "Ajouter";
+?>
