db602ba84c8b243396634471deaa44602cb53bf3
vmaury <vmaury>
2005-09-26 16:27:46 +0000

    added import management

---
+++ etoil/www/admin-import.php
@@ -0,0 +1,10 @@
+<?php
+$title = "Gestion des utilisateurs";
+$db = true;
+$mod = false;
+$feedback = $focus = array();
+include("setup.php");
+
+echo "Bienvenue sur la page d'importation directe des tracés";
+
+?>
