f03374ea8cd1e30ef4ee39b34c5c2fbd00e5ecca
vmaury <vmaury>
2006-03-08 18:22:09 +0000

    ca gaze

--- etoil/www/pop_det_parc.php
+++ etoil/www/pop_det_parc.php
@@ -1,8 +1,8 @@
 <?php 
 /* 
-AU début ce fichier était affiché via une popup. Mais comm e cela merde avec IE, il est affiché dans le map en mode "unique"
+AU dï¿½ut ce fichier ï¿½ait affichï¿½via une popup. Mais comm e cela merde avec IE, il est affichï¿½dans le map en mode "unique"
 
-$title = "Détails du parcours";
+$title = "Dï¿½ails du parcours";
 $db = true;
 include("setup.php");
 */
@@ -13,14 +13,14 @@ include("setup.php");
 $reqcust ="select * from parcours where parcours_id=".$_SESSION['pid']." " ;
 $req=db_query($reqcust);
 $tbValChp=db_fetch_array($req);
-// maintenant ces infos sont stockées en base, plutot qu ede les recalculer à chaque fois ...
+// maintenant ces infos sont stockï¿½s en base, plutot qu ede les recalculer ï¿½chaque fois ...
 $smarty->assign('parc_name',$tbValChp['parcours_name']);
-$smarty->assign('parc_length',$tbValChp['parcours_length']);
+$smarty->assign('parc_length',round($tbValChp['parcours_length']/1000,1));
 $smarty->assign('parc_time',($tbValChp['parcours_time']>0 ? $tbValChp['parcours_time'] : floor($tbValChp['parcours_length']/$_SESSION['vitmoy']/100)/10));
 $smarty->assign('denivtot',$tbValChp['parcours_deniv']);
 
-// on ne refait les req geomatiques qu'n édition du parcours
-if ($_REQUEST['editpc']) { // on ne refait des calculs et requete sur la base qu'en édition 
+// on ne refait les req geomatiques qu'n ï¿½ition du parcours
+if ($_REQUEST['editpc']) { // on ne refait des calculs et requete sur la base qu'en ï¿½ition 
 	$nbpoints=$db->getone("select NumPoints(parcours_geom) from parcours where parcours_id=".$_SESSION['pid']);
 	$parc_length=floor($db->getone("select Length2D(parcours_geom) from parcours where parcours_id=".$_SESSION['pid']));
 	
@@ -28,7 +28,7 @@ if ($_REQUEST['editpc']) { // on ne refait des calculs et requete sur la base qu
 	//$parc_geom=$db->getone("select asEWKT(parcours_geom) from parcours where parcours_id=".$_SESSION['pid']);
 	//echo $parc_geom."\n";
 	
-	// calcul du dénivellé positif
+	// calcul du dï¿½ivellï¿½positif
 	$nbpaltOk=0;
 	$cst_distm=50;
 	for ($i=1;$i<=$nbpoints;$i++) {
@@ -59,7 +59,7 @@ if ($_REQUEST['editpc']) { // on ne refait des calculs et requete sur la base qu
 	
 	$denivtot=0;
 	$i=0;
-	if (is_array($datas)) {	// on a des données d'altitude
+	if (is_array($datas)) {	// on a des donnï¿½s d'altitude
 		$deniv_ok=true;
 		foreach ($datas as $h) {
 			$i++;
@@ -69,12 +69,12 @@ if ($_REQUEST['editpc']) { // on ne refait des calculs et requete sur la base qu
 	}
 		
 	$tb_infparc.='<table border="1"><thead><th colspan="2">';
-	$tb_infparc.='Elements recalculés par le SIG </th></thead><tr><td>';
+	$tb_infparc.='Elements recalculï¿½ par le SIG </th></thead><tr><td>';
 	
 	
 	if ($deniv_ok) {
 		$denivtot=floor($denivtot);
-		$tb_infparc.= "Ci-dessous la courbe de dénivellé telle qu'elle vient d'être calculée en temps réel par le SIG: <br/>";
+		$tb_infparc.= "Ci-dessous la courbe de denivele telle qu'elle vient d'etre calculee en temps reel par le SIG: <br/>";
 //		$_SESSION['datas']=$datas; // pour imgpostgraph.php
 //		$tb_infparc.='<IMG SRC="imgpostgraph.php">';
 		$_SESSION['datas']=$datas; // pour imgpostgraph.php
@@ -84,51 +84,51 @@ if ($_REQUEST['editpc']) { // on ne refait des calculs et requete sur la base qu
 		include ("imgpostgraph.php"); 
 		$tb_infparc.='<IMG SRC="'.$chem_web_genimgtmp.$img_name.'">';
 		$tb_infparc.=echochphid("parcours_deniv_fname",$img_name,false);
-		$tb_infparc.='<br><input name="parcours_deniv_cgraph" type="checkbox" value="'.$filetogen.'" checked/> <b>enregistrer ce graphe</b> de dénivellé pour ce parcours';
+		$tb_infparc.='<br><input name="parcours_deniv_cgraph" type="checkbox" value="'.$filetogen.'" checked/> <b>enregistrer ce graphe</b> de denivelle pour ce parcours';
 		
 
 	} else {
-		$tb_infparc.= " Aucune information de dénivellé disponible pour ce parcours";
+		$tb_infparc.= " Aucune information de dï¿½ivellï¿½disponible pour ce parcours";
 	}
 	$tb_infparc.="</td><td>";
-	$tb_infparc.="<br/><b>&#149; Longueur</b> du parcours \"recalculée\" par le SIG (en m) :<b> ".$parc_length."</b>";
+	$tb_infparc.="<br/><b>&#149; Longueur</b> du parcours \"recalculee\" par le SIG (en m) :<b> ".$parc_length."</b>";
 	$tb_infparc.='<br><input name="parcours_length_cvalue" type="checkbox" value="'.$parc_length.'" '.($tbValChp['parcours_length']>0 ? "" : "checked").'/> <b>enregistrer cette valeur</b> de longueur pour ce parcours';
 	$timec=floor($parc_length/100/$_SESSION['vitmoy'])/10;
-	$tb_infparc.="<br/><br/><b>&#149; Temps</b> du parcours \"recalculé\" par le SIG, avec une vitesse moyenne de ".$_SESSION['vitmoy']." km/h: <b> ".$timec." h </b>";
+	$tb_infparc.="<br/><br/><b>&#149; Temps</b> du parcours \"recalcul&eacute;\" par le SIG, avec une vitesse moyenne de ".$_SESSION['vitmoy']." km/h: <b> ".$timec." h </b>";
 	$tb_infparc.='<br><input name="parcours_time_cvalue" type="checkbox" value="'.$timec.'" '.($tbValChp['parcours_time']>0 ? "" : "checked").'/> <b>enregistrer ce temps</b> pour ce parcours';
-	$tb_infparc.="<br/><small><u>N.B. :</u>la vitesse moyenne d'une discipline est reparamétrable, demandez à l'admistrateur système</small>"; 
+	$tb_infparc.="<br/><small><u>N.B. :</u>la vitesse moyenne d'une discipline est reparametrable, demandez a l'administrateur systeme</small>"; 
 	
 	if ($deniv_ok) {
-		$tb_infparc.="<br/><br/><b>&#149; Dénivellé</b> du parcours \"recalculée\" par le SIG (en m) :<b> ".$denivtot."</b>";
-		// genere une chcbox, précochée si valeur en bdd =0; non cochée sinon
-		$tb_infparc.='<br><input name="parcours_deniv_cvalue" type="checkbox" value="'.$denivtot.'" '.($tbValChp['parcours_deniv']>0 ? "" : "checked").'/> <b>enregistrer cette valeur</b> de dénivellé pour ce parcours';
+		$tb_infparc.="<br/><br/><b>&#149; Denivele</b> du parcours \"recalculee\" par le SIG (en m) :<b> ".$denivtot."</b>";
+		// genere une chcbox, prï¿½ochï¿½ si valeur en bdd =0; non cochï¿½ sinon
+		$tb_infparc.='<br><input name="parcours_deniv_cvalue" type="checkbox" value="'.$denivtot.'" '.($tbValChp['parcours_deniv']>0 ? "" : "checked").'/> <b>enregistrer cette valeur</b> de denivele pour ce parcours';
 		
 		}
-	$tb_infparc.="<br/<br/><small>les valeurs recalculées par le SIG peuvent différer ce ces mesurées sur le terrain. C'est pourquoi il est possible de les ressaisir manuellement ci-dessous</small>";
+	$tb_infparc.="<br/<br/><small>les valeurs recalculï¿½s par le SIG peuvent diffï¿½er ce ces mesurï¿½s sur le terrain. C'est pourquoi il est possible de les ressaisir manuellement ci-dessous</small>";
 	
 	$tb_infparc.='<br/<br/><input type="reset" value="ANNULER LES MODIFICATIONS" class="redbutton"> <br/><input type="submit" onclick="ChgMeth(); document.f.action=\'maj_table.php\'; document.f.submit()" class="redbutton" name="toto" value="!! ENREGISTRER LES MODIFICATIONS D\'ATTRIBUTS DE CE PARCOURS !!"/>&nbsp;';
 	
 	$tb_infparc.="</td><tr></table>";
 
 	//
-} // fin si on est en édition 
+} // fin si on est en edition
 
 $usercid=RecupLib("users","login","user_id",$_SESSION['me']);
 
 $booluscreaconnect= ($tbValChp['parcours_uscrea']==$usercid || $_SESSION['admin']);
 
-// bouton edition affiché
+// bouton edition affichï¿½
 if ($booluscreaconnect  && !$_REQUEST['editpc']) {
 	$tb_infparc.='<input id="edithid" type="hidden" name="editpc" value="">
 	<input type="submit"  onclick="document.getElementById(\'edithid\').value=\'true\'; document.f.submit()" class="redbutton" name="toto" value="!! MODIFIER LES ATTRIBUTS DE CE PARCOURS !!"/>';
-}
 
+}
 $tb_infparc.='<TABLE class="table">';
 $tb_infparc.='<thead><th>Attribut</th><th>Valeur</th></thead>';
 
 
 
-// Création et Initialisation des propriétés des objets PYAobj
+// Crï¿½tion et Initialisation des propriï¿½ï¿½ des objets PYAobj
 
 $reqLChp="SELECT NM_CHAMP from $TBDname where NM_TABLE='parcours' AND NM_CHAMP!='$NmChDT' AND (TYPEAFF!='HID' OR (TT_PDTMAJ!='' AND TT_PDTMAJ!= NULL)) ORDER BY ORDAFF, LIBELLE";
 
@@ -142,14 +142,13 @@ while ($CcChp=db_fetch_row($rq1)) { // boucles sur les champs
   $PYAObj->NmTable="parcours";
   $NM_CHAMP=$CcChp[0];
   $PYAObj->NmChamp=$NM_CHAMP;
-  if (!$_REQUEST['editpc']) $PYAObj->TypEdit="C"; // en consultation seule si on a pas appelé l'édition
+  if (!$_REQUEST['editpc']) $PYAObj->TypEdit="C"; // en consultation seule si on a pas appelï¿½l'ï¿½ition
   $PYAObj->InitPO();
-  if ($PYAObj->TypeAff=="POPL") $poplex=true; // s'il existe au moins une edition en popup liée
+  if ($PYAObj->TypeAff=="POPL") $poplex=true; // s'il existe au moins une edition en popup liï¿½
   $PYAObj->DirEcho=false;
-  $PYAObj->ValChp=$tbValChp[$NM_CHAMP]; // si pas création (edit ou copy recup la val)
+  $PYAObj->ValChp=$tbValChp[$NM_CHAMP]; // si pas crï¿½tion (edit ou copy recup la val)
   
-// en acces anonyme, donc on cache tous ceux-là
-if (!$booluscreaconnect) {
+// en acces anonyme, donc on cache tous ceux-lï¿½if (!$booluscreaconnect) {
      switch ($NM_CHAMP) {
   	case "parcours_id" :
 	case "parcours_discp" :
@@ -164,18 +163,19 @@ if (!$booluscreaconnect) {
 	case "parcours_uscrea" :
 	case "parcours_usmaj" :
 	
-	$PYAObj->TypeAff="HID";
+	if (!$booluscreaconnect) $PYAObj->TypeAff="HID";
+		
 	break;
     }
-}
 
-// mise en consult forcée de certains champs si pas admin  
+
+// mise en consult forcï¿½ de certains champs si pas admin  
   switch ($NM_CHAMP) {
   	case "parcours_discp":
 	case "parcours_idsource":
 	case "parcours_marqimp":
 	case "parcours_user":
-		if (!$_SESSION['admin']) $PYAObj->TypEdit="C";
+		if (!$_SESSION['admin']) $PYAObj->TypeAff="HID";
 	break;
 	
   }
@@ -189,26 +189,76 @@ if (!$booluscreaconnect) {
      $tb_infparc.='<TR  class="table"><TD class="colattribut">'.$PYAObj->Libelle;
      if ($booluscreaconnect) $tb_infparc.="<br/><small>".$PYAObj->Comment."</small>";
      $tb_infparc.="</TD>\n<TD>";
-     $tb_infparc.=$PYAObj->EchoEdit($PYAObj->TypEdit=="C" ? false : true);
+     switch ($NM_CHAMP) {
+     	case "parcours_length":
+     		if ($PYAObj->TypEdit=="C") {
+     			$tb_infparc.=round($tbValChp[$NM_CHAMP]/1000,1)." km"; }
+     		else 	{
+     			$tb_infparc.=$PYAObj->EchoEdit(true);
+     			$tb_infparc.="<br/><small>Si vous changez la valeur, saisissez-la en <b><u>metres</b></u>; elle s'affichera en km pour les internautes</small>";
+     			}
+     		break;
+     	
+     	case "parcours_guides":
+		if ($PYAObj->TypEdit=="C") {
+			$rqg="SELECT guid_nom,guid_editeur,guid_collection,guid_photocouv,guid_descript,guid_url,guid_ref,guid_isbn,guid_prix,guid_etat from guides where guid_id=".$tbValChp['parcours_guides'];
+			$TbPYAOG=RTbVChPO($rqg,$dbname);
+			foreach ($TbPYAOG as $PYAOG) {
+				$tb_infparc.=str_replace(":|",": ",$PYAOG)."<br/>";
+			}
+		}  // fin si mode consultation
+		else $tb_infparc.=$PYAObj->EchoEdit($PYAObj->TypEdit=="C" ? false : true);
+		
+    		break;
+     	
+     	case "parcours_balisage":
+		if ($PYAObj->TypEdit=="C") {
+			$rqg="SELECT balis_nom,balis_desc,balis_color,balis_ico,balis_icoscomp from balisages where balis_id=".$tbValChp['parcours_balisage'];
+			$TbPYAOG=RTbVChPO($rqg,$dbname);
+			foreach ($TbPYAOG as $PYAOG) {
+				$tb_infparc.=str_replace(":|",": ",$PYAOG)."<br/>";
+			}
+		}  // fin si mode consultation
+		else $tb_infparc.=$PYAObj->EchoEdit($PYAObj->TypEdit=="C" ? false : true);
+		
+    		break;
+     	
+     	case "parcours_gestionnaire":
+		if ($PYAObj->TypEdit=="C") {
+			$rqg="SELECT gest_nom,gest_resp,gest_desc,gest_adresse1,gest_adresse2,gest_commune,gest_tel,gest_tel2,gest_fax,gest_email,gest_web from gestionnaires where gest_id=".$tbValChp['parcours_gestionnaire'];
+			$TbPYAOG=RTbVChPO($rqg,$dbname);
+			foreach ($TbPYAOG as $PYAOG) {
+				$tb_infparc.=str_replace(":|",": ",$PYAOG)."<br/>";
+			}
+		}  // fin si mode consultation
+		else $tb_infparc.=$PYAObj->EchoEdit($PYAObj->TypEdit=="C" ? false : true);
+		
+    		break;
+     	
+     	default :
+     	$tb_infparc.=$PYAObj->EchoEdit($PYAObj->TypEdit=="C" ? false : true);
+     	break;
+     
+     }
      $tb_infparc.="</TD>\n</TR>"; //finit la ligne du tableau
    }
    elseif ($_REQUEST['editpc'])  $tb_infparc.=$PYAObj->NmChamp.":".$PYAObj->EchoEditAll(true)."|"; // !!!!!!!!!!!!!!!! /
 
-  } // fin while
+  } // fin while ie boucle sur les champs
 $tb_infparc.="</table>";
 
 if ($_REQUEST['editpc']) { // on est en edition, on peut valider)
-	if ($poplex) JSpopup(); // s'il existe au moins une edition en popup liée colle le code d'ouverture d'une popup
+	if ($poplex) JSpopup(); // s'il existe au moins une edition en popup liï¿½ colle le code d'ouverture d'une popup
 	// pour amact_table.php
 	$tb_infparc.=echochphid("modif","1",false);
-	$tb_infparc.=echochphid("keyf",$_SESSION['pid']."_",false); // clé pour fichier
-	$tb_infparc.=echochphid("key","parcours_id=".$_SESSION['pid'],false); // clé pour modif
+	$tb_infparc.=echochphid("keyf",$_SESSION['pid']."_",false); // clï¿½pour fichier
+	$tb_infparc.=echochphid("key","parcours_id=".$_SESSION['pid'],false); // clï¿½pour modif
 	$tb_infparc.=echochphid("adrr_majt",$_SERVER["PHP_SELF"],false);
 	$tb_infparc.=echochphid("NM_TABLE","parcours",false);
 	// bouton supprimer pour l'admin seult 
 	if ($_SESSION['admin']) $tb_infparc.='<br/><span class="redbutton"><br/><input type="checkbox" name="delete" value="on">!!! Supprimer ce parcours !!!<br/></span><br/>';
 	
-	$tb_infparc.='<input type="reset" value="ANNULER LES MODIFICATIONS" class="redbutton"> &nbsp; &nbsp; <input type="submit" onclick="ChgMeth(); document.f.action=\'maj_table.php\'; document.f.submit()" class="redbutton" name="toto" value="!! ENREGISTRER LES MODIFICATION SD\'ATTRIBUTS DE CE PARCOURS !!"/>&nbsp;';
+	$tb_infparc.='<input type="reset" value="ANNULER LES MODIFICATIONS" class="redbutton"> &nbsp; &nbsp; <input type="submit" onclick="ChgMeth(); document.f.action=\'maj_table.php\'; document.f.submit()" class="redbutton" name="toto" value="!! ENREGISTRER LES MODIFICATIONS D\'ATTRIBUTS DE CE PARCOURS !!"/>&nbsp;';
 }	
 $smarty->assign('tb_infparc',$tb_infparc);
 
--- etoil/www/pop_leg_ign.php
+++ etoil/www/pop_leg_ign.php
@@ -7,16 +7,53 @@
 <link rel="StyleSheet" type="text/css" href="etoil.css" />
 </head>
 <body style="margin:40px">
-<H1>Mentions l&#233;gales concernant <br>les fonds de carte IGN utilis&#233;s sur e-toil:</H1>
+
+<h1>Mentions l&#233;gales</H1>
+<br><br>
+<h2>éditeur du site</h2>
+
+<h3>directeur de la publication </h3>
+Comit&eacute; r&eacute;gional d'&eacute;quitation<br>
+Association loi 1901<br>
+Le Chateau - Tour n°4 <br>
+19230 POMPADOUR<br>
+tel: 33 (0)5 55 73 60 99<br>
+Courriel : limousin[-at-]ffe.com<br>
+Président: Emmanuel Quittet<br><br>
+
+<h2>conditions d'utilisation des chemins</h2>
+
+<h3>article L. 361-1 du code de l'environnement :</h3>
+
+  <p>La circulation des piétons sur les voies et chemins inscrits au  
+plan départemental des itinéraires de promenade et de randonnée, ou  
+ceux identifiés pour les chemins privés, après conventions passées  
+avec les propriétaires de ces chemins, par les communes et les  
+fédérations de randonneurs agréées s'effectue librement, dans le  
+respect des lois et règlements de police et des droits des riverains.</p>
+    <p>Les maires, en vertu de leur pouvoir de police, peuvent, le cas  
+échéant, réglementer les conditions d'utilisation de ces itinéraires.</p>
+    <p>La responsabilité civile des propriétaires ruraux et forestiers 
+ne saurait être engagée au titre des dommages causés ou subis à  
+l'occasion de la circulation des piétons ou de la pratique  
+d'activités de loisirs qu'en raison de leurs actes fautifs.</p>
+
+<h3>N.B.</h3>
+<p>Le CRE Limousin assure la mise en ligne d'informations qui lui sont  
+transmises par les concepteurs et gestionnaires d'itinéraires de  
+randonnées. il n'est pas responsable de l'accessibilité de ces  
+itinéraires et de leur état d'entretien.
+</p>
+<H2>Fonds cartographiques IGN</H2>
 <br>
-<H2>Scan 1000</H2>
+<H3>Scan 1000</H3>
 <br>
 Licence n&#176; 2005cubc044<br><br>
-<H2>Scan Regional</H2>
+<H3>Scan Regional</H3>
 <br>
 Region Limousin<br>
 Licence  n&#176; 2006cubc0156<br><br>
-<H2>Scan 100</H2>
+<H3>Scan 100</H3>
 <br>
 Region Limousin<br>
 Licence 2006cubc0155<br><br>
@@ -29,7 +66,6 @@ Le Chateau - Tour n°4
 Pour tout complément d'information, contactez: <a href="mailto:ar-limousin@ign.fr">ar-limousin@ign.fr</a>
 <br><br>
 <h3>Reproduction Interdite</h3><br><br>
-
 <a href="#" onclick="self.close()" class="button" style="margin-left:100px">Fermer cette fenêtre</a>
 </body>
 </html>
